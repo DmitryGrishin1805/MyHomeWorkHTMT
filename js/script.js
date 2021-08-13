081025d95a351797c2b988cb0bfa7b476f8e80fb
Dmitry Grishin <dg9261115311@gmail.com>
2021-08-13 17:30:42 +0300

    ddddd

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-"use sctrict";
+"use strict";
 
-// alert('Hello');
+ alert('Hello');
 
 const result = confirm(" are you here");
 console.log(result);
 const answer = prompt("dfv tcnm 18", "18");
+
 console.log(typeof(answer));
